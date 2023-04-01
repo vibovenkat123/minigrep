63a10e069e2a22272785083b60343c34f8252c91
Vaibhav Venkat <vibovenkat@gmail.com>
2023-04-01 13:33:02 -0500

    feat: add error handling for the arguments

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,23 @@
-use std::env;
+use std::{cmp::Ordering, env, fs, process::exit};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let query = &args[1];
-    let path = &args[2];
-    println!("Finding {}", query);
-    println!("In file {}", path);
+
+    let (query, path): (String, String) = match args.len().cmp(&3) {
+        Ordering::Less => {
+            println!("Not enough arguments");
+            exit(1);
+        }
+        Ordering::Greater => {
+            println!("Too much arguments");
+            exit(1);
+        }
+        Ordering::Equal => {
+            let query_arg = &args[1];
+            let path_arg = &args[2];
+            (path_arg.to_string(), query_arg.to_string())
+        }
+    };
+    println!("Finding {}", &query);
+    println!("In file {}", &path);
 }
