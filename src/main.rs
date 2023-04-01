7e060f093434691d699706bc9dd334edf3691088
Vaibhav Venkat <vibovenkat@gmail.com>
2023-04-01 13:55:58 -0500

    feat: break it up into modules

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,36 @@
-use std::{cmp::Ordering, env, fs, process::exit};
-
+use std::{cmp::Ordering, env, fs, process};
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let (query, path): (String, String) = match args.len().cmp(&3) {
-        Ordering::Less => {
-            println!("Not enough arguments");
-            exit(1);
-        }
-        Ordering::Greater => {
-            println!("Too much arguments");
-            exit(1);
-        }
-        Ordering::Equal => {
-            let query_arg = &args[1];
-            let path_arg = &args[2];
-            (query_arg.to_string(), path_arg.to_string())
+    let config = Config::new(&args);
+    println!("Finding {}", &config.query);
+    println!("In file {}", &config.path);
         }
     };
     println!("Finding {}", &query);
     println!("In file {}", &path);
+struct Config {
+    query: String,
+    path: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        let (query, path): (String, String) = match args.len().cmp(&3) {
+            Ordering::Less => {
+                println!("Not enough arguments");
+                process::exit(1);
+            }
+            Ordering::Greater => {
+                println!("Too much arguments");
+                process::exit(1);
+            }
+            Ordering::Equal => {
+                let query_arg = &args[1];
+                let path_arg = &args[2];
+                (query_arg.to_string(), path_arg.to_string())
+            }
+        };
+        Config { query, path }
+    }
 }
